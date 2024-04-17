fdb3c0f9f3d3a50ed00a1df4d24e83090d3fcbf5
Siddharth Nalamalpu <453434@beaverton.k12.or.us>
2024-04-17 12:11:13 -0700

    Layout

--- src/chessGame.java
+++ src/chessGame.java
@@ -1,22 +1,43 @@
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class chessGame {
 	JFrame frame = new JFrame();
+	int xWidth=800;
+	int yWidth=800;
+	JButton[][] button = new JButton[8][8];
+	Container center = new Container();
+	
 	
 	public chessGame() {
-		frame.setSize(800,600);
+		//Sets size of screen
+		frame.setSize(xWidth,yWidth);
 		frame.setLayout(new BorderLayout());
-		
+		center.setLayout(new GridLayout(8,8));
+		//i is for row
+		for (int i = 0; i < button.length; i++) {
+			//j is for column
+			for (int j = 0; j < button[0].length; j++) {
+				button[j][i] = new JButton(j+""+i);
+				center.add(button[j][i]);
+			}
+		}
+		frame.add(center, BorderLayout.CENTER);
+
 		//Sets program to close when tab is closed
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//Allows user to see what has been created
 		frame.setVisible(true);
+
 	}
 	
 	public static void main(String[] args) {
 		new chessGame();
 	}
+	
 
 }
---
+++ src/chessPanel.java
@@ -0,0 +1,16 @@
+import java.awt.Frame;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class chessPanel extends JPanel {
+
+	public chessPanel() {
+		super();
+	}
+	public void paintComponent(Graphics g) {
+		//Calling JPanel's paint component
+		super.paintComponent(g);
+		//draw my stuff
+	}
+}
